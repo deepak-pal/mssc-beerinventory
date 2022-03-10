fefb9c0379cc484ab7821f0e6845850a1c4bfeb4
Deepak Pal <deepak.pal@technia.com>
2022-03-10 06:31:06 +0530

    Initial Commit

---
+++ mssc-beerinventory/mssc-beerinventory/src/main/java/com/inic/springframework/msscbeerinventory/MsscBeerinventoryApplication.java
@@ -0,0 +1,13 @@
+package com.inic.springframework.msscbeerinventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerinventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscBeerinventoryApplication.class, args);
+	}
+
+}
---
+++ mssc-beerinventory/mssc-beerinventory/src/test/java/com/inic/springframework/msscbeerinventory/MsscBeerinventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inic.springframework.msscbeerinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerinventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
